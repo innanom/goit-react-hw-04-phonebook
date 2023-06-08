8d507a01ede633d84c4ebb22c194a9ba5469033c
Inna Nomerovkina <inkosof@gmail.com>
2023-06-08 23:36:36 +0300

    fixed App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,24 +5,23 @@ import Filter from '../Filter/Filter'
 import { nanoid } from 'nanoid';
 import { Container, Title, TitleContacts } from './App.styled';
 
+const initialState = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
+
 export const App = () => {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
-  const [filter, setFilter] = useState('');
-
-
-  useEffect(() => {
-    const contacts = localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse(contacts);
-
-    if (parsedContacts) {
-      setContacts(parsedContacts);
+  const [contacts, setContacts] = useState(() => {
+    if (localStorage.getItem('contacts')) {
+      return JSON.parse(localStorage.getItem('contacts'));
     }
-  }, []);
+    return initialState;
+
+  });
+    
+    const [filter, setFilter] = useState(''); 
 
   useEffect(() => {
     localStorage.setItem('contacts', JSON.stringify(contacts));
@@ -36,11 +35,9 @@ const addContact = ({ name, number }) => {
     };
     if (contacts.some(contact => contact.name.toLowerCase() === name.toLowerCase())) {
       alert(`${name} is already in contacts`);
-   
+      return;
     }
-    else {
-         setContacts(prevContacts => [contact, ...prevContacts]);
-    }
+      setContacts(prevContacts => [contact, ...prevContacts]);
   };
 
   const deleteContact = contactId => {
