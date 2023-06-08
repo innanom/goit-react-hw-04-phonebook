e8fffac869c80e5b098638314d7a704e5c8407ce
Inna Nomerovkina <inkosof@gmail.com>
2023-06-08 23:47:04 +0300

    fix coment

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -55,64 +55,6 @@ export const Form = ({onSubmit}) => {
         );
 }
 
-// class Form extends Component {
-//     state = {
-//         name: '',
-//         number: ''
-//     }
-
-//     handleChange = event => {
-//     const { name, value } = event.currentTarget;
-
-//     this.setState({
-//       [name]: value,
-//     })
-//     }
-    
-//     handleSubmit = event => {
-//         event.preventDefault();
-//         const { name, number } = this.state;
-//         this.props.onSubmit({name, number});
-//         this.reset();
-//     }
-//      reset = () => {
-//         this.setState({
-//         name: '',
-//         number:''})
-//     }
-
-//     render() {
-//         return (
-//           <FormContacts onSubmit={this.handleSubmit}>
-//             <LabelForm>Name
-//             <InputForm
-//               type="text"
-//               name="name"
-//               placeholder='Enter your name'
-//               value={this.state.name}
-//               onChange={this.handleChange}
-//               pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-//               title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-//               required
-//             />
-//            </LabelForm>
-//            <LabelForm>Number
-//               <InputForm
-//                 type="tel"
-//                 name="number"
-//                 placeholder='Enter your telephone number'
-//                 value={this.state.number}
-//                 onChange={this.handleChange}
-//                 pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-//                 title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-//                 required
-//               />
-//            </LabelForm>
-//           <BtnForm type='submit'>Add contact</BtnForm>
-//          </FormContacts>
-//         );
-//     }
-// }
 
 Form.propTypes = {
   name: PropTypes.string,
